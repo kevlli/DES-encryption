7d9b3e04e777f1c85124a7b5a2bf83e2b1b006b5
Matthew <mtang20@stuy.edu>
2021-12-13 16:46:18 -0500

    12/13 updated DESencrypt

--- DESencrypt.py
+++ DESencrypt.py
@@ -8,18 +8,19 @@ pc1 = [57,   49,    41,   33,    25,    17,    9,
     63,   55,    47,   39,    31,    23,   15,
     7,   62,    54,   46,    38,    30,   22,
     14,    6,    61,   53,    45,    37,   29,
-    21,   13,     5,   28,    20,   12,    4,]
+    21,   13,     5,   28,    20,   12,    4]
+
+pc2 = [14,    17,   11,    24,     1,    5,
+    3,    28,   15,     6,    21,   10,
+    23,    19,   12,     4,    26,    8,
+    16,     7,   27,    20,    13,    2,
+    41,    52,   31,    37,    47,   55,
+    30,    40,   51,    45,    33,   48,
+    44,    49,   39,    56,    34,   53,
+    46,    42,   50,    36,    29,   32]
+
 def encrypt(plaintext, key):
-    key1 = ""
-    for i in range(0,len(pc1)):
-        #python starts at 0, table starts a 1, so subtract 1
-        key1 += key[pc1[i] - 1]
-    print(key1)
-    #print(key[57])
-    #print(key[49])
-    #print(key[41])
-    
-    
+    key1 = permutation(key, pc1)
     c0 = ""
     d0 = ""
     for i in range(0, len(key1)):
@@ -27,11 +28,12 @@ def encrypt(plaintext, key):
             c0 += key1[i]
         else:
             d0 += key1[i]
-    print (c0)
-    print (d0)
+    #print (c0)
+    #print (d0)
     
     
     klistc = []
+    klistd = []
     shifts = [1,1,2,2,2,2,2,2,1,2,2,2,2,2,2,1]
     for i in range(0, 16):
         if i == 0:
@@ -40,10 +42,40 @@ def encrypt(plaintext, key):
             klistc.append(leftshift(klistc[i - 1]))
             if shifts[i] == 2:
                 klistc[i] = leftshift(klistc[i])
-    for i in range(len(klistc)):
-        print (klistc[i])
-        print ("")
-            
+    #for i in range(len(klistc)):
+        #print (klistc[i])
+        #print ("")
+
+    for i in range(0, 16):
+        if i == 0:
+            klistd.append(leftshift(d0))
+        else:
+            klistd.append(leftshift(klistd[i - 1]))
+            if shifts[i] == 2:
+                klistd[i] = leftshift(klistd[i])
+    #for i in range(len(klistd)):
+        #print (klistd[i])
+        #print ("")
+
+    klist = []
+    for i in range(0,16):
+        subkey = klistc[i] + klistd[i]
+
+        print(subkey) #subkey before permutation
+        
+    #    subkey = permutation(subkey, pc2)
+    #    klist.append(subkey)
+    #    print(klist[i])
+
+
+def permutation(string, ptable):
+    pstring = ""
+    for i in range(0,len(ptable)):
+        #python starts at 0, table starts at 1, so subtract 1
+        pstring += string[ptable[i] - 1]
+    return pstring
+
+    
 def leftshift(string):
     newstring = ""
     for i in range(len(string)):
@@ -52,6 +84,8 @@ def leftshift(string):
         else:
             newstring += string[i + 1]
     return (newstring)
+
+permutation("11100001100110010101010111111010101011001100111100011110",pc2)
     
         
     
