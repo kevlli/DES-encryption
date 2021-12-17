0a9f3f7c6574a4be0daa8232305946ddcb8551ca
Matthew <mtang20@stuy.edu>
2021-12-17 15:13:09 -0500

    final permutation box

--- DESmatthew.py
+++ DESmatthew.py
@@ -24,6 +24,24 @@ ip = [58, 50, 42, 34, 26, 18, 10, 2, 60, 52, 44,
     61, 53, 45, 37, 29, 21, 13, 5, 63, 55, 47, 39,
     31, 23, 15, 7]
 
+ebit = [32,     1,    2,     3,     4,    5,
+                  4,     5,    6,     7,     8,    9,
+                  8,     9,   10,    11,    12,   13,
+                 12,    13,   14,    15,    16,   17,
+                 16,    17,   18,    19,    20,   21,
+                 20,    21,   22,    23,    24,   25,
+                 24,    25,   26,    27,    28,   29,
+                 28,    29,   30,    31,    32,    1]
+
+fp = [40,     8,   48,    16,    56,   24,    64,   32,
+    39,     7,   47,    15,    55,   23,    63,   31,
+    38,     6,   46,    14,    54,   22,    62,  30,
+    37,     5,   45,    13,    53,   21,    61, 29,
+    36,     4,   44,    12,    52,   20,    60,   28,
+    35,     3,   43,    11,    51,   19,    59,   27,
+    34,     2,   42,    10,    50,   18,    58,   26,
+    33,     1,   41,     9,    49,   17,    57,   25]
+
 s1 = [ [14,  4,  13,  1,   2, 15,  11,  8,   3, 10,   6, 12,   5,  9,   0,  7],
           [0, 15,   7,  4,  14,  2,  13,  1,  10,  6,  12, 11,   9,  5,   3,  8],
           [4,  1,  14,  8,  13,  6,   2, 11,  15, 12,   9,  7,   3, 10,   5,  0],
@@ -64,6 +82,26 @@ s8 =  [ [13,  2,   8,  4,   6, 15,  11,  1,  10,  9,   3, 14,   5,  0,  12,  7],
           [7, 11,   4,  1,   9, 12,  14,  2,   0,  6,  10, 13,  15,  3,   5,  8],
           [2,  1,  14,  7,   4, 10,   8, 13,  15, 12,   9,  0,   3,  5,   6, 11] ]
 
+psbox = [16,   7,  20,  21,
+                         29,  12,  28,  17,
+                          1,  15,  23,  26,
+                          5,  18,  31,  10,
+                          2,   8,  24,  14,
+                         32,  27,   3,   9,
+                         19,  13,  30,   6,
+                         22,  11,   4,  25]
+
+sboxes = []
+
+sboxes.append(s1)
+sboxes.append(s2)
+sboxes.append(s3)
+sboxes.append(s4)
+sboxes.append(s5)
+sboxes.append(s6)
+sboxes.append(s7)
+sboxes.append(s8)
+
 def encrypt(plaintext, key):
     key1 = permutation(key, pc1)
     slist = split(key1)
@@ -93,22 +131,24 @@ def encrypt(plaintext, key):
     #    print (klistd[i])
     #    print("")
 
-
-    
-    print("keys:")
     klist = []
-    for i in range(0,16):
+    for i in range(16):
         subkey = klistc[i] + klistd[i]
         subkey = permutation(subkey, pc2)
         klist.append(subkey)
-        #print keys
-        print(klist[i])
-        
-    print("ip permutated plaintext:")
-    plaintext1 = permutation(plaintext, ip)
-    print(plaintext1)
-    print("split plaintext:")
-    print(split(plaintext1))
+        #print(klist[i])
+
+    #Step 2
+    plainip = permutation(plaintext, ip)
+    #print(plainip)
+    #divide into two halve
+    ipsplit = split(plainip)
+    ipL = [ ipsplit[0] ]
+    ipR = [ ipsplit[1] ]
+    for i in range (16):
+        print(int(ipL[i]) ^ int(functionf(ipR[i], klist[i])))
+
+
 
 def permutation(string, ptable):
     pstring = ""
@@ -136,4 +176,27 @@ def split(string):
             slist[1] += string[i]
     return slist
 
-encrypt("0001001100110100010101110111100110011011101111001101111111110001", "0001001100110100010101110111100110011011101111001101111111110001")
+def functionf(text, key):
+    expandedtext = permutation(text, ebit)
+    xored = int(key,2) ^ int(expandedtext,2)
+    xored = format(xored, "b")
+
+    while (len(xored) != 48) : #adds leading 0s to first 6 bits
+        xored = '0' + xored
+    compacted = ""
+    for i in range(8):
+        compacted += sbox(xored[i * 6: (i + 1) * 6], sboxes[i])
+    compacted = permutation(compacted, psbox)
+    return compacted
+    
+def sbox(text, s):
+    row = int(text[0]) * 2 + int(text[5])
+    column = int(text[1]) * 8 + int(text[2]) * 4 + int(text[3]) * 2 + int(text[4])
+    value = format(s[row][column], "b")
+    while (len(value) != 4):
+        value = '0' + value #adds prefixing 0s for some bits (i.e. 0001)
+    return value
+
+
+
+#encrypt("0001001100110100010101110111100110011011101111001101111111110001", "0001001100110100010101110111100110011011101111001101111111110001")
